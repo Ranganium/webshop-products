1f54b6feae05e8506845ec92c23761fbe076b482
Ranganium <j.korsgaard.kristensen@gmail.com>
2026-01-28 23:28:21 +0100

    ...

--- app.js
+++ app.js
@@ -1,38 +1,47 @@
 "use strict";
 
-// ===== APP INITIALISERING =====
-// Start app når DOM er loaded (hele HTML siden er færdig med at indlæse)
-document.addEventListener("DOMContentLoaded", initApp);
+const product1 = {
+  title: "Fjallraven - Foldsack No. 1 Backpack",
+  price: 849,
+  inStock: true,
+  image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png",
+  description:
+    "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+  category: "shirts",
+};
 
-// Initialize the app - sæt event listeners og hent data
-function initApp() {
-  console.log("App initialized");
-}
-// Product 1
-const productTitle = "Fjallraven - Foldsack No. 1 Backpack";
-const productPrice = 849;
-const inStock = true;
+const product2 = {
+  title: "Mens Casual Premium Slim Fit T-Shirts",
+  price: 179,
+  inStock: true,
+  image: "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_t.png",
+  description:
+    "The color could be slightly different between on the screen and in practice. / Please note that body builds vary by person, therefore, detailed size information should be reviewed below on the product description.",
+  category: "shirts",
+};
 
-// Product 2
-const productTitle2 = "Mens Casual Premium Slim Fit T-Shirts";
-const productPrice2 = 179;
-const inStock2 = false;
+console.log(product1);
+console.log(product2);
 
-const testSection = document.querySelector("#test");
-const html = `
-<div>
-  <h3>Test: Produkt information</h3>
-  <p><strong>Titel:</strong> ${productTitle}</p>
-  <p><strong>Pris:</strong> ${productPrice}</p>
-  <p><strong>På lager:</strong> ${inStock}</p>
-  <p><strong>Titel:</strong> ${productTitle2}</p>
-  <p><strong>Pris:</strong> ${productPrice2}</p>
-  <p><strong>På lager:</strong> ${inStock2}</p>
-</div>`;
-testSection.insertAdjacentHTML("beforeend", html);
-
-if (inStock) {
-  console.log(`${productTitle} er på lager!`);
+let stockText;
+let stockClass;
+if (product1.inStock) {
+  stockText = "På lager";
+  stockClass = "in-stock";
 } else {
-  console.log(`${productTitle} er desværre ikke på lager!`);
+  stockText = "Udsolgt";
+  stockClass = "out-of-stock";
 }
+
+const html = /*html*/ `
+  <article class="product-card">
+    <img src="${product1.image}" class="product-image">
+    <div class="product-info">
+      <h2 class="product-name">${product1.title}</h2>
+      <p class="product-description">${product1.description}</p>
+      <p class="product-price">${product1.price} kr</p>
+      <span class="product-stock ${stockClass}">${stockText}</span>
+    </div>
+  </article>
+`;
+document.querySelector("#test").insertAdjacentHTML("beforeend", html);
