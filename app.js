eee4df6f1176a84a1b14ad541b86aa00d6fc8bef
Ranganium <j.korsgaard.kristensen@gmail.com>
2026-01-28 22:38:24 +0100

    stuff

--- app.js
+++ app.js
@@ -8,3 +8,31 @@ document.addEventListener("DOMContentLoaded", initApp);
 function initApp() {
   console.log("App initialized");
 }
+// Product 1
+const productTitle = "Fjallraven - Foldsack No. 1 Backpack";
+const productPrice = 849;
+const inStock = true;
+
+// Product 2
+const productTitle2 = "Mens Casual Premium Slim Fit T-Shirts";
+const productPrice2 = 179;
+const inStock2 = false;
+
+const testSection = document.querySelector("#test");
+const html = `
+<div>
+  <h3>Test: Produkt information</h3>
+  <p><strong>Titel:</strong> ${productTitle}</p>
+  <p><strong>Pris:</strong> ${productPrice}</p>
+  <p><strong>På lager:</strong> ${inStock}</p>
+  <p><strong>Titel:</strong> ${productTitle2}</p>
+  <p><strong>Pris:</strong> ${productPrice2}</p>
+  <p><strong>På lager:</strong> ${inStock2}</p>
+</div>`;
+testSection.insertAdjacentHTML("beforeend", html);
+
+if (inStock) {
+  console.log(`${productTitle} er på lager!`);
+} else {
+  console.log(`${productTitle} er desværre ikke på lager!`);
+}
