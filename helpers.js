428bf6e4cfbc4229eabea91c717be45f0a66b21d
Ranganium <j.korsgaard.kristensen@gmail.com>
2026-01-31 17:10:18 +0100

    done ned til 7.

---
+++ helpers.js
@@ -0,0 +1,15 @@
+export const getExcerpt = (text) => text.substring(0, 100) + "...";
+
+export function getStockStatus(inStock) {
+  if (inStock) {
+    return {
+      text: "På lager",
+      class: "in-stock",
+    };
+  } else {
+    return {
+      text: "Udsolgt",
+      class: "out-of-stock",
+    };
+  }
+}
